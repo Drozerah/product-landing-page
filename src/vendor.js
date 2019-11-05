5f19ee48e0067bd862dcf4611a1f286e012a4506
Drozerah <drozerah@gmail.com>
2019-11-05 17:03:17 +0100

    feat: import matirialize.js
    - create src/vendor.js
    - import src/vendor.js in webpack.common.js
    - src/scss/_materialize.scss:
    	- update componets imports
    - src/index.html:
    	- add test content

---
+++ src/vendor.js
@@ -0,0 +1,57 @@
+/**
+ * 3rd party libraries import
+ */
+/**
+ * Materialize.js Components
+ */
+
+// Materialize global import
+// import 'materialize-css'
+
+// Use Materialize as modular components
+// @{link} https://github.com/Dogfalo/materialize/issues/5958#issuecomment-439634071
+
+// Components
+import 'materialize-css/js/cash'
+
+import 'materialize-css/js/component'
+// import './app/materialize-hacks/js/component' //~~ HACK
+
+import 'materialize-css/js/global'
+// import 'materialize-css/js/anime.min.js'
+
+// import 'materialize-css/js/modal'
+// import './app/materialize-hacks/js/modal' //~~ HACK
+
+import 'materialize-css/js/waves'
+// import 'materialize-css/js/forms'
+
+// import 'materialize-css/js/select'
+// import './app/materialize-hacks/js/select' //~~ HACK
+
+// import 'materialize-css/js/dropdown'
+// import './app/materialize-hacks/js/dropdown' //~~ HACK
+
+// import 'materialize-css/js/autocomplete'
+// import 'materialize-css/js/buttons'
+// import 'materialize-css/js/cards'
+// import 'materialize-css/js/carousel'
+
+// import 'materialize-css/js/characterCounter'
+// import './app/materialize-hacks/js/characterCounter' //~~ HACK
+
+// import 'materialize-css/js/chips'
+// import 'materialize-css/js/collapsible'
+// import 'materialize-css/js/datepicker'
+// import 'materialize-css/js/materialbox'
+// import 'materialize-css/js/parallax'
+// import 'materialize-css/js/pushpin'
+// import 'materialize-css/js/range'
+// import 'materialize-css/js/scrollspy'
+// import 'materialize-css/js/sidenav'
+// import 'materialize-css/js/slider'
+// import 'materialize-css/js/tabs'
+// import 'materialize-css/js/tapTarget'
+// import 'materialize-css/js/timepicker'
+// import 'materialize-css/js/toasts'
+// import 'materialize-css/js/tooltip'
