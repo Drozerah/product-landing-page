cfbf8e7c7669f4f229e5e3b026d23691abdb9816
Drozerah <drozerah@gmail.com>
2019-11-08 20:21:05 +0100

    fix: waves effet issue when css is purged
    - add waves effect css classes to purgecss whitelist

--- purgecss-whitelist.js
+++ purgecss-whitelist.js
@@ -2,4 +2,20 @@
  * Purgecss whitelist
  */
 
-module.exports = ['sidenav-overlay']
+module.exports = [
+  'sidenav-overlay',
+  'sidenav-trigger',
+  'waves-effect',
+  'waves-ripple',
+  'waves-light',
+  // 'waves-redwaves-yellow',
+  'waves-orange',
+  // 'waves-purple',
+  // 'waves-green',
+  // 'waves-teal',
+  'waves-notransition',
+  'waves-circle',
+  'waves-input-wrapper',
+  'waves-button-input',
+  'waves-block'
+]
