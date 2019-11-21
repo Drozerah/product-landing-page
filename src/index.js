7e9f79d8ba08fb7a2cacdc5b37e759431688fba0
Drozerah <drozerah@gmail.com>
2019-11-21 19:06:37 +0100

    test: fix DOMContentLoaded event

--- src/index.js
+++ src/index.js
@@ -45,28 +45,29 @@ console.log('app started!')
 //   // Run the application
 //   run()
 // })
-
-function browserSupportsAllFeatures () {
-  return window.document.Symbol
-}
-
-function loadScript (src, done) {
-  var js = document.createElement('script')
-  js.src = src
-  js.onload = function () {
-    done()
+document.addEventListener('DOMContentLoaded', () => {
+  function browserSupportsAllFeatures () {
+    return window.document.Symbol
   }
-  js.onerror = function () {
-    done(new Error('Failed to load script ' + src))
-  }
-  document.head.appendChild(js)
-}
 
-if (browserSupportsAllFeatures()) {
+  function loadScript (src, done) {
+    var js = document.createElement('script')
+    js.src = src
+    js.onload = function () {
+      done()
+    }
+    js.onerror = function () {
+      done(new Error('Failed to load script ' + src))
+    }
+    document.head.appendChild(js)
+  }
+
+  if (browserSupportsAllFeatures()) {
   // Browsers that support all features run `run()` immediately.
-  run()
-} else {
+    run()
+  } else {
   // All other browsers loads polyfills and then run `run()`.
-  const src = 'https://polyfill.io/v3/polyfill.min.js?flags=gated&features=Array.from%2CArray.prototype.forEach%2CSymbol%2CSymbol.iterator%2CSymbol.asyncIterator%2CMutationObserver%2CArray.prototype.%40%40iterator'
-  loadScript(src, run)
-}
+    const src = 'https://polyfill.io/v3/polyfill.min.js?flags=gated&features=Array.from%2CArray.prototype.forEach%2CSymbol%2CSymbol.iterator%2CSymbol.asyncIterator%2CMutationObserver%2CArray.prototype.%40%40iterator'
+    loadScript(src, run)
+  }
+})
